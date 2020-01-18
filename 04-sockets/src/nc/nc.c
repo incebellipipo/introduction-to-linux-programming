413c94645acecae9a3d1b6a2625773b603d632a9
Emir Cem Gezer <emircem.gezer@gmail.com>
2020-01-18 12:56:36 +0300

    chore: add scrafold for improved socket server

---
+++ 04-sockets/src/nc/nc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int main(int argc, char** argv) {
+
+
+  return 0;
+}
