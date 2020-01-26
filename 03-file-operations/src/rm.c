da18d72a667290439cd7a7895ed3565ebe195414
Emir Cem Gezer <emircem.gezer@gmail.com>
2020-01-27 00:35:31 +0300

    feat: implement rm

--- 03-file-operations/src/rm.c
+++ 03-file-operations/src/rm.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main(int argc, char** argv) {
 
-  fprintf(stderr, "not implemented yet!"); 
+  if(argc == 2) {
+    if(remove(argv[1]) != 0) {
+      perror("remove");
+    }
+  } else {
+    fprintf(stderr, "I need args!\n");
+  }
   
   return 0;
 }
