f6f23d9ff01e9552d8e2a1fd420bc64714ca9c23
Emir Cem Gezer <emircem.gezer@gmail.com>
2020-01-28 12:36:21 +0300

    feat: add unix socket client

--- 04-sockets/src/samples/unix-client.c
+++ 04-sockets/src/samples/unix-client.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
+
 
 int main(int argc, char** argv) {
+  fprintf(stdout, "starting\n");
+  struct sockaddr_un addr;
+  char buf[BUFSIZ];
 
-  fprintf(stderr, "Not implemented yet");
+  int fd, cl, rc;
+
+  fprintf(stdout, "socket creating...\n");
+  if( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0 ) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+  memset(&addr, 0 , sizeof(addr));
+  addr.sun_family = AF_UNIX;
+  strcpy(addr.sun_path, "/var/tmp/dsocket");
+  unlink(addr.sun_path);
+
+  connect(fd, (struct sockaddr*)&addr, sizeof(addr));
+
+  write(fd, argv[1],strlen(argv[1]));
+
+  close(fd); 
   return 0;
 }
