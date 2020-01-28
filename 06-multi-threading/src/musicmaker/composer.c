8989935be17799b1deaa189c68496693c7757f39
Mahmut Karaca <mahmutkaraca95@gmail.com>
2020-01-28 15:48:45 +0300

    feat: add some music to 06-multi-threading

--- 06-multi-threading/src/musicmaker/composer.c
+++ 06-multi-threading/src/musicmaker/composer.c
@@ -1,37 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <musicmaker/sound.h>
+#include <musicmaker/util.h>
 #include <pthread.h>
 #include <unistd.h>
 
-int main(int argc, char** argv) {
+void playNote(int freq, int duration) {
+  pthread_t th;
+  struct play_sound_args *note_args = malloc(1 * sizeof(struct play_sound_args));
+  note_args->duration = duration;
+  note_args->freq = freq;
 
-  pthread_t note_A;
-  pthread_t note_Cs;
-  pthread_t note_E;
-  int duration = 1;
-  struct play_sound_args node_a_args;
-  node_a_args.duration=duration;
-  node_a_args.freq=440;
+  pthread_create(&th, NULL, (void *) &play_sound, note_args);
+  pthread_detach(th);
+  sleep_ms(duration);
+}
+
+void *back_instrument(void *d) {
+  sleep_ms(200);
+  playNote(NOTE_D3, 200);
+  sleep_ms(200);
+  playNote(NOTE_D3, 200);
+  sleep_ms(200);
+  playNote(NOTE_D3, 200);
+  sleep_ms(200);
+  playNote(NOTE_D3, 200);
+  sleep_ms(200);
+  playNote(NOTE_C3S, 200);
+  sleep_ms(200);
+  playNote(NOTE_C3S, 200);
+  sleep_ms(200);
+  playNote(NOTE_C3S, 200);
+  sleep_ms(200);
+  playNote(NOTE_C3S, 200);
+  sleep_ms(200);
+  playNote(NOTE_D3, 200);
+  sleep_ms(200);
+  playNote(NOTE_D3, 200);
+  sleep_ms(200);
+  playNote(NOTE_D3, 200);
+  sleep_ms(200);
+  playNote(NOTE_D3, 200);
+  sleep_ms(200);
+  playNote(NOTE_A2, 200);
+  sleep_ms(200);
+  playNote(NOTE_A2, 200);
+  sleep_ms(200);
+  playNote(NOTE_D3, 200);
+  sleep_ms(200);
+  playNote(NOTE_D3, 200);
+  sleep_ms(200);
+}
+
+void *main_instrument(void *d) {
+  playNote(NOTE_A4, 400);
+
+  playNote(NOTE_E4, 200);
+  playNote(NOTE_F4, 200);
+  playNote(NOTE_G4, 400);
+
+  playNote(NOTE_F4, 200);
+  playNote(NOTE_E4, 200);
+  playNote(NOTE_D4, 400);
+
+  playNote(NOTE_D4, 200);
+  playNote(NOTE_F4, 200);
+  playNote(NOTE_A4, 400);
+
+  playNote(NOTE_G4, 200);
+  playNote(NOTE_F4, 200);
+  playNote(NOTE_E4, 600);
+
+  playNote(NOTE_F4, 200);
+  playNote(NOTE_G4, 400);
+  playNote(NOTE_A4, 400);
+
+  playNote(NOTE_F4, 400);
+
+  playNote(NOTE_D4, 400);
+
+  playNote(NOTE_D4, 400);
   
-  struct play_sound_args node_b_args;
-  node_b_args.duration=duration;
-  node_b_args.freq=580;
+  sleep_ms(600);
 
-  struct play_sound_args note_e_args;
-  note_e_args.duration = duration;
-  note_e_args.freq = 660;
+
+}
+
+int main(int argc, char** argv) {
+  int duration = 1;
+  pthread_t main_instr;
+  pthread_t back_instr;
 
   open_audio();
 
-  pthread_create(&note_A, NULL, (void*)&play_sound, (struct play_sound_args*)&node_a_args);
-  pthread_detach(note_A);
-  pthread_create(&note_Cs, NULL, (void*)&play_sound, (struct play_sound_args*)&node_b_args);
-  pthread_detach(note_Cs);
-  pthread_create(&note_E, NULL, (void*)&play_sound, (struct play_sound_args*)&note_e_args);
-  pthread_detach(note_E);
-  // wait a bit, what else you gotta do?
-  sleep(duration * 5 + duration);
+  pthread_create(&main_instr, NULL, main_instrument, NULL);
+  pthread_detach(main_instr);
+
+  pthread_create(&back_instr, NULL, back_instrument, NULL);
+  pthread_detach(back_instr);
+
+  sleep(10);
   close_audio();
   return 0;
 }
